ccb53f15aa2399ea5817fa51da8cf2ef9cce7d3a
Aaron Young <luozhu2021@gmail.com>
2025-03-27 10:23:50 +0800

    Update .dumirc.ts

--- .dumirc.ts
+++ .dumirc.ts
@@ -35,6 +35,7 @@ export default defineConfig({
   },
   hash: true,
   exportStatic: {},
+  ...(process.env.NODE_ENV === 'development' ? {} : { ssr: {} }),
   headScripts: process.env.NODE_ENV === 'development' ? [] : [
     { src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js', async: true, crossorigin: 'anonymous' }
   ],
