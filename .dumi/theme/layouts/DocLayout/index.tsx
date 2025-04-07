238d30a7a0be51a1a521b2937c97265b58cb463e
Aaron Young <luozhu2021@gmail.com>
2025-04-07 11:25:31 +0800

    perf(theme): 为 DocLayout 中的图片添加懒加载属性
    - 在 DocLayout 组件中的 Picture 组件上添加 loading="lazy" 属性
    - 这样可以提高页面加载性能，实现图片的按需加载

--- .dumi/theme/layouts/DocLayout/index.tsx
+++ .dumi/theme/layouts/DocLayout/index.tsx
@@ -129,6 +129,7 @@ const DocLayout: FC = () => {
               inputPosition="top"
               theme="preferred_color_scheme"
               lang="zh-CN"
+              loading="lazy"
             />
           </article>
           <ContentFooter />
