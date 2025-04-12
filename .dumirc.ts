5530225c5f6ef7a3eda6fb5cbeab7364c1ac814b
Aaron Young <luozhu2021@gmail.com>
2025-04-12 17:22:10 +0800

    refactor(nav): update "我的" section link to "/myenv/ubuntu"
    - Changed the link for the "我的" section in the navigation to point to "/myenv/ubuntu"
    - This update specifies the Ubuntu environment under the "我的" section

--- .dumirc.ts
+++ .dumirc.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     nav: [
       { title: '英语', link: '/english' },
       { title: '算法', link: '/algorithm' },
-      { title: '我的', link: '/myenv' },
+      { title: '我的', link: '/myenv/ubuntu' },
       { title: '周刊', link: 'https://weekly.zisheng.pro' },
     ],
     hd: { rules: [] },
