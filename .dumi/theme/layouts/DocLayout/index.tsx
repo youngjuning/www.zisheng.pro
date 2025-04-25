98d895946f0cff406507a1336ab52928139fa91a
Aaron Young <luozhu2021@gmail.com>
2025-04-25 08:27:16 +0800

    refactor: remove Adsense components from DocLayout

--- .dumi/theme/layouts/DocLayout/index.tsx
+++ .dumi/theme/layouts/DocLayout/index.tsx
@@ -94,25 +94,7 @@ const DocLayout: FC = () => {
         {showSidebar && <Sidebar />}
         <Content>
           <article>
-            {/* zisheng_top */}
-            <Adsense
-              className="adsbygoogle adsbygoogle-bottom"
-              style={{ display: 'block'}}
-              data-ad-client="ca-pub-5641491107630454"
-              data-ad-slot="1645676004"
-              data-ad-format="auto"
-              data-full-width-responsive="true"
-            />
             {outlet}
-            {/* zisheng_bottom */}
-            <Adsense
-              className="adsbygoogle adsbygoogle-bottom"
-              style={{ display: 'block'}}
-              data-ad-client="ca-pub-5641491107630454"
-              data-ad-slot="5702084207"
-              data-ad-format="auto"
-              data-full-width-responsive="true"
-            />
             <Giscus
               id="comments"
               repo="youngjuning/zisheng.pro"
